bb6b01efb8d4cf686058b1ca5bdd5ad292160725
Larissa Santos <79234739+larisantosdev@users.noreply.github.com>
2025-11-12 23:22:34 +0000

    adicionando banco de dados, SQLTools SQLT  e MySQL

---
+++ Controllers/UsuarioControler.cs
@@ -0,0 +1,15 @@
+namespace ModuloAPI.Controllers
+{
+    /// <summary>
+    /// Classe vazia de exemplo para o controlador de usuário.
+    /// </summary>
+    public class UsuarioControler
+    {
+        // Construtor vazio
+        public UsuarioControler()
+        {
+        }
+
+        // Adicione propriedades e métodos conforme necessário
+    }
+}
